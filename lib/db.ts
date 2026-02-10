190b628617fe53791c431375e33ae7fec7a51575
Subhajit Choudhury <122729012+Subhajit-FD@users.noreply.github.com>
2026-02-11 00:15:47 +0530

    deploy test

--- lib/db.ts
+++ lib/db.ts
@@ -12,7 +12,9 @@ async function dbConnect(): Promise<void> {
     return;
   }
   try {
-    const db = await mongoose.connect(process.env.MONGO_URI!);
+    const db = await mongoose.connect(process.env.MONGO_URI!, {
+      dbName: "blog-website",
+    });
     connection.isConnected = db.connections[0].readyState;
     console.log("MongoDB connected");
   } catch (error) {
